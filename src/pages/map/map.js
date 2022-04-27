76f7c9de1446fa68c0c8281c5f27bb265b06354d
ksiek127 <ketain@interia.pl>
2022-04-27 18:43:47 +0200

    mainpage initial draft, basic routing

---
+++ src/pages/map/map.js
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { Component } from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import map from "../../images/map.jpg";
+import "../../style/map.css";
+import Sector from "../sector/sector";
+
+class Map extends Component {
+  // constructor(){
+  //   super();
+  // }
+
+  render() {
+    return (
+        <div className="map">
+          <img
+              className="mapImg"
+              alt="Mapa lasu"
+              src={map}
+            />
+        </div>
+    );
+  }
+}
+
+export default Map;
---
+++ src/pages/sector/plot.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { Component } from "react";
+
+class Plot extends Component{
+    constructor(){
+        super();
+    }
+
+    render(){
+        return(
+            <div className="plotContainer">
+
+            </div>
+        )
+    }
+}
+
+export default Plot;
