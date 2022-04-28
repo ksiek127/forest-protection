755b6c1d5c7379268c3a490233ee6b0269adefbe
pawlew2k <pawlew2k@gmail.com>
2022-04-28 16:31:08 +0200

    backend file configurations done

---
+++ backend/flask_api/README.md
@@ -0,0 +1,7 @@
+### run
+    python map.py
+
+#### endpoints:
+    127.0.0.1:5000/map
+    127.0.0.1:5000/sector?sector_name=<sector_name>
+
---
+++ backend/flask_api/map.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, request
+import json
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    info = "./map<br>" \
+           "./sector"
+    return info
+
+@app.route('/map', methods=['GET', 'POST'])
+def map_view():
+    directory_path = '../../utils/json_maps/filled_100x100.json' # TODO probably changed when we create some databases
+    with open(directory_path) as file:
+        return json.load(file)
+    # name = request.args.get('forest_name')
+
+@app.route('/sector', methods=['GET', 'POST'])
+def sector_view():
+    directory_path = '../../utils/json_maps/filled_100x100.json'  # TODO probably changed when we create some databases
+    with open(directory_path) as file:
+        name = request.args.get('sector_name')
+        sectors = json.load(file)["sectors"]
+        for sector in sectors:
+            if sector['sector_name'] == name:
+                return sector
+    # name = request.args.get('forest_name')
+    # sector_name = request.args.get('forest_name')
+
+if __name__ == '__main__':
+    app.run(debug=True)
