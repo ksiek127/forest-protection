9a253088f0121bdebba82c89050ced5bc0c6dabb
pawlew2k <pawlew2k@gmail.com>
2022-04-28 17:03:03 +0200

    changed level_of_threat to threat_level and added random_threat_level()

--- backend/generator_configuration/map_generator.py
+++ backend/generator_configuration/map_generator.py
@@ -16,7 +16,7 @@ def random_float(_from, _to, rounded: int) -> float:
 class MapGenerator:
     def __init__(self, name, height, width, size):
         list_of_sectors = self.random_initial_sector(height, width)
-        list_of_sectors = self.empty_initial_sector(height, width)
+        # list_of_sectors = self.empty_initial_sector(height, width)
         self.map = Map(forest_name=name,
                        height=height,
                        width=width,
@@ -32,7 +32,7 @@ class MapGenerator:
                     Sector(sector_name=str(y) + "_" + str(x),
                            position=[y, x],
                            time=datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
-                           level_of_threat="low",
+                           threat_level="low",
                            temperature=None,
                            wind_direction=None,
                            wind_speed=None,
@@ -50,7 +50,7 @@ class MapGenerator:
                     Sector(sector_name=str(y) + "_" + str(x),
                            position=[y, x],
                            time=datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
-                           level_of_threat="low",
+                           threat_level=self.random_threat_level(),
                            temperature=random_float(_from=15, _to=20, rounded=2),
                            wind_direction=self.random_wind_direction(),
                            wind_speed=random_float(_from=0, _to=1.5, rounded=2),
@@ -59,6 +59,11 @@ class MapGenerator:
                            aerosol_concentration=random_float(_from=20, _to=80, rounded=0),
                            carbon_dioxide_concentration=random_float(_from=200, _to=400, rounded=0)))
         return list_of_sectors
+
+    @staticmethod
+    def random_threat_level() -> str:
+        threat_level = ["none", "low", "medium", "high"]
+        return threat_level[randint(0, len(threat_level) - 1)]
 
     @staticmethod
     def random_wind_direction() -> str:
--- backend/generator_configuration/sector.py
+++ backend/generator_configuration/sector.py
@@ -6,7 +6,7 @@ class Sector:
     sector_name: str
     position: [int, int]
     time: datetime.datetime
-    level_of_threat: str # {low, medium, high} TODO uncompleted, probably more
+    threat_level: str # {none, low, medium, high} TODO uncompleted, probably more
     temperature: float # [°C]
     wind_direction: str # {N, NE, E, SE, S, SW, W, NW}
     wind_speed: float # [m/s]
